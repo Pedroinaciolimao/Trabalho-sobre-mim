596c21b4533b88b88fb567a91cd5197ac922f55a
Pedroinaciolimao <pedroinaciolimaorocha@gmail.com>
2025-07-05 20:21:35 -0300

    Primeiro upload do site

---
+++ inicio.js
@@ -0,0 +1,204 @@
+let inicio1=document.querySelector(".home")
+inicio1.addEventListener("mouseover", mudatranparente)
+inicio1.addEventListener("mouseout", mudanormal)
+
+function mudatranparente(){
+    inicio1.style.background="#515151"
+
+}
+function mudanormal(){
+    inicio1.style.background=""
+}
+
+let curtinhos=document.querySelector(".curtos")
+curtinhos.addEventListener("mouseover", mudatranparente2)
+curtinhos.addEventListener("mouseout", mudanormal2)
+
+function mudatranparente2(){
+    curtinhos.style.background="#515151"
+
+}
+function mudanormal2(){
+    curtinhos.style.background=""
+}
+let inscritos2=document.querySelector(".inscritos")
+inscritos2.addEventListener("mouseover", mudatranparente20)
+inscritos2.addEventListener("mouseout", mudanormal20)
+
+function mudatranparente20(){
+    inscritos2.style.background="#515151"
+
+}
+function mudanormal20(){
+    inscritos2.style.background=""
+}
+
+
+let tudo1=document.querySelector("#tudo")
+tudo1.addEventListener("mouseover", mudatranparente14)
+tudo1.addEventListener("mouseout", mudanormal14)
+
+function mudatranparente14(){
+    tudo1.style.background="#515151"
+
+}
+function mudanormal14(){
+    tudo1.style.background=""
+}
+
+
+
+
+let musica1=document.querySelector("#musica")
+musica1.addEventListener("mouseover", mudatranparente3)
+musica1.addEventListener("mouseout", mudanormal3)
+
+function mudatranparente3(){
+    musica1.style.background="#515151"
+
+}
+function mudanormal3(){
+   musica1.style.background=""
+}
+
+let nome3=document.querySelector("#axe")
+nome3.addEventListener("mouseover", mudatranparente4)
+nome3.addEventListener("mouseout", mudanormal4)
+
+function mudatranparente4(){
+    nome3.style.background="#515151"
+
+}
+function mudanormal4(){
+   nome3.style.background=""
+}
+
+
+
+let nome4=document.querySelector("#mixes")
+nome4.addEventListener("mouseover", mudatranparente5)
+nome4.addEventListener("mouseout", mudanormal5)
+
+function mudatranparente5(){
+    nome4.style.background="#515151"
+
+}
+function mudanormal5(){
+   nome4.style.background=""
+}
+
+
+
+let nome5=document.querySelector("#jogos")
+nome5.addEventListener("mouseover", mudatranparente6)
+nome5.addEventListener("mouseout", mudanormal6)
+
+function mudatranparente6(){
+    nome5.style.background="#515151"
+
+}
+function mudanormal6(){
+   nome5.style.background=""
+}
+
+
+
+
+let nome6=document.querySelector("#funkc")
+nome6.addEventListener("mouseover", mudatranparente7)
+nome6.addEventListener("mouseout", mudanormal7)
+
+function mudatranparente7(){
+    nome6.style.background="#515151"
+
+}
+function mudanormal7(){
+   nome6.style.background=""
+}
+
+
+
+
+let nome7=document.querySelector("#forro")
+nome7.addEventListener("mouseover", mudatranparente8)
+nome7.addEventListener("mouseout", mudanormal8)
+
+function mudatranparente8(){
+    nome7.style.background="#515151"
+
+}
+function mudanormal8(){
+   nome7.style.background=""
+}
+
+
+
+
+let nome8=document.querySelector("#parodia")
+nome8.addEventListener("mouseover", mudatranparente9)
+nome8.addEventListener("mouseout", mudanormal9)
+
+function mudatranparente9(){
+    nome8.style.background="#515151"
+
+}
+function mudanormal9(){
+   nome8.style.background=""
+}
+
+
+
+let nome9=document.querySelector("#rowanatk")
+nome9.addEventListener("mouseover", mudatranparente10)
+nome9.addEventListener("mouseout", mudanormal10)
+
+function mudatranparente10(){
+    nome9.style.background="#515151"
+
+}
+function mudanormal10(){
+   nome9.style.background=""
+}
+
+
+
+
+let nome10=document.querySelector("#satira")
+nome10.addEventListener("mouseover", mudatranparente11)
+nome10.addEventListener("mouseout", mudanormal11)
+
+function mudatranparente11(){
+    nome10.style.background="#515151"
+
+}
+function mudanormal11(){
+   nome10.style.background=""
+}
+
+
+
+let nome11=document.querySelector("#libertadores")
+nome11.addEventListener("mouseover", mudatranparente12)
+nome11.addEventListener("mouseout", mudanormal12)
+
+function mudatranparente12(){
+    nome11.style.background="#515151"
+
+}
+function mudanormal12(){
+   nome11.style.background=""
+}
+
+
+
+let nome12=document.querySelector("#aovivo")
+nome12.addEventListener("mouseover", mudatranparente13)
+nome12.addEventListener("mouseout", mudanormal13)
+
+function mudatranparente13(){
+    nome12.style.background="#515151"
+
+}
+function mudanormal13(){
+   nome12.style.background=""
+}
